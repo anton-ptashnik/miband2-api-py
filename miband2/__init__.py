233b5118b089f729654d23c1765f926027e336d9
Anton Ptashnik <iavtomator@gmail.com>
2024-05-29 19:29:24 +0300

    update metadata

--- miband2/__init__.py
+++ miband2/__init__.py
@@ -1,7 +1,7 @@
 """
-MiBand2 communication library
+Python API for communicating with MiBand2
 """
-__version__ = "0.0.1"
+__version__ = "0.1.1"
 
 from .band2 import Band2, NotificationType
 from .authsession import Key
